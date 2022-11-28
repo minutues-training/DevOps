11fe306da257ed247e338783687cdf6e4a794411
pramod <pramod.desai@minutuscomputing.com>
2022-11-28 18:03:48 +0530

    azure scripts

---
+++ infra/azure-scripts/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "ef7481c9-0872-49de-a1af-a1a6b5bcb82a"
+  tenant_id       = "f7a314d2-1d44-43bd-81a7-2442a1d99314"
+  client_id       = "9aa1f7c6-fd29-4a28-a481-acef02253cdd"
+  client_secret   = "XmK8Q~RQC-Sk.g6GgwSQEKvCArKN474eEy6u7c6Y"
+  features {}
+
+}
