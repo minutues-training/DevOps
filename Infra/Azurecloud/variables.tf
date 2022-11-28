58dbbc978e872c3270fd0de760227a19445ba282
pramod <pramod.desai@minutuscomputing.com>
2022-11-28 18:53:28 +0530

    azure-scripts

---
+++ Infra/Azurecloud/variables.tf
@@ -0,0 +1,15 @@
+variable "different_subnets" {
+  type    = number
+  default = 6
+}
+
+variable "multiple_machine" {
+  type    = number
+  default = 6
+
+}
+
+variable "type" {
+  default = "Standard_A1_v2"
+
+}
